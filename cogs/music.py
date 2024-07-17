fcc8b01a6276b5b465261aced0e80b4320d35f82
ulf <jonas14costa@gmail.com>
2024-07-17 19:36:56 -0300

    - Fix currentOption  & remove hidden

--- cogs/music.py
+++ cogs/music.py
@@ -137,17 +137,15 @@ class YTDLSource(discord.PCMVolumeTransformer):
                 "Erro ao adquirir video, tentando encontrar nova na lista. Erro: %s", e
             )
             raise
+        currentOptions = ffmpeg_options.copy()
+        if extraArgs:
+            currentOptions["before_options"] = (
+                f"{currentOptions.get('before_options', '')} {extraArgs}"
+            )
         if PERFORMANCE_MODE:
-            currentOptions = ffmpeg_options
-            if extraArgs:
-                currentOptions["before_options"] = (
-                    f"{currentOptions.get('before_options', '')} {extraArgs}"
-                )
             return cls(discord.FFmpegPCMAudio(filename, **currentOptions), data=data)
         else:
-            return cls(
-                discord.FFmpegPCMAudio(filename, **ffmpeg_options_loudnorm), data=data
-            )
+            return cls(discord.FFmpegPCMAudio(filename, **currentOptions), data=data)
 
 
 class Music(commands.Cog):
@@ -461,9 +459,9 @@ class Music(commands.Cog):
                 ctx.voice_client.source.volume = volume / 100
                 await ctx.send("Volume trocado para {}%".format(volume))
 
-    @commands.command(aliases=["ff", "fastforward"], hidden=True)
+    @commands.command(aliases=["ff", "fastforward"])
     async def fast_forward(self, ctx: commands.Context, seconds: int):
-        """Avança o áudio atual por um número específico de segundos"""
+        """Avança o áudio atual por um número específico de segundos do video. Sempre é considerado a partir do inicio e não o momento atual"""
         if ctx.voice_client.is_playing() or ctx.voice_client.is_paused():
             ctx.voice_client._player.source = await YTDLSource.from_url(
                 self.queue[ctx.guild.id],
