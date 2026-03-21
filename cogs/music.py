12d32ef0431f59d2bcf876c5bc1245d2b173647e
ulf <jonas14costa@gmail.com>
2026-03-21 15:51:25 +0000

    Cahnge audio source default

--- cogs/music.py
+++ cogs/music.py
@@ -147,8 +147,8 @@ class YTDLSource(discord.PCMVolumeTransformer):
             )
 
         try:
-            # audio_source = discord.FFmpegPCMAudio(filename, **currentOptions)
-            audio_source = discord.FFmpegOpusAudio(filename, **currentOptions)
+            audio_source = discord.FFmpegPCMAudio(filename, **currentOptions)
+            # audio_source = discord.FFmpegOpusAudio(filename, **currentOptions)
             return cls(audio_source, data=data)
         except Exception as e:
             log.error("Erro ao criar FFmpeg Audio: %s", e)
